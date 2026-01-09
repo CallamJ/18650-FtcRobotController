dd30e26d5a95a008b5a6ec73f9769daa8a89e6e6
Callam Jomaa <apps@callamjomaa.com>
2026-01-09 04:11:58 -0500

    add launcher, hood, and turret components

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Launcher.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.components;
+
+import org.firstinspires.ftc.teamcode.hardware.SmartMotor;
+import org.firstinspires.ftc.teamcode.hardware.controllers.ControlAlgorithm;
+import org.firstinspires.ftc.teamcode.hardware.controllers.PID;
+
+public class Launcher {
+    private final SmartMotor motor;
+
+    private final ControlAlgorithm controller;
+
+    public static double kP = 0.01, kI = 0, kD = 0.005, kF = 0.02, tolerance = 0.1;
+
+    public static float ticksPerDegree = (8192f/360f);
+
+    private double targetVelocity;
+
+    public Launcher(SmartMotor motor) {
+        this.motor = motor;
+        this.controller = new PID.Builder()
+                .setKP(() -> kP)
+                .setKI(() -> kI)
+                .setKD(() -> kD)
+                .setKF(() -> kF)
+                .setTolerance(tolerance)
+                .build();
+    }
+
+    public void tick(){
+        motor.setPower(controller.calc(targetVelocity, getVelocity()));
+    }
+
+    public void setTargetVelocity(double targetVelocity) {
+        this.targetVelocity = targetVelocity;
+    }
+
+    public double getTargetVelocity() {
+        return targetVelocity;
+    }
+
+    public double getVelocity() {
+        return motor.getVelocity() / ticksPerDegree;
+    }
+
+    public double getPower() {
+        return motor.getPower();
+    }
+
+    public void stop() {
+        setTargetVelocity(0);
+    }
+
+    public boolean isStopped() {
+        return targetVelocity == 0;
+    }
+
+}
