9503a3adc054f11c7887fd32cee1e70bfc61a9ee
Callam Jomaa <apps@callamjomaa.com>
2026-01-22 20:25:10 -0500

    add drivebase hardware configuration for pedro pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveBase.java
@@ -4,55 +4,72 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.Drivetrain;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.Mecanum;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.ftc.localization.localizers.PinpointLocalizer;
 import com.pedropathing.localization.Localizer;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.core.OpModeCore;
 import org.firstinspires.ftc.teamcode.drive.DriveBaseMotorConfig;
 import org.firstinspires.ftc.teamcode.utilities.Pose;
 
 @Configurable
 public class DriveBase {
 
-    private final Drivetrain drivetrain;
+    private Drivetrain drivetrain;
     private final Localizer localizer;
-    private final Follower follower;
+    private Follower follower;
     public static float TRANSLATIONAL_VELOCITY_MULTIPLIER = 40f;
     public static float HEADING_VELOCITY_MULTIPLIER = 3f;
 
     private double powerFactor = 1;
 
-    public DriveBase(HardwareMap hardwareMap, DriveBaseMotorConfig config) {
-        MecanumConstants mConstants = new MecanumConstants();
+    public DriveBase(HardwareMap hardwareMap, DriveBaseMotorConfig config, boolean startFollower) {
 
-        mConstants.leftFrontMotorName = config.leftFrontName;
-        mConstants.rightFrontMotorName = config.rightFrontName;
-        mConstants.leftRearMotorName = config.leftRearName;
-        mConstants.rightRearMotorName = config.rightRearName;
-
-        mConstants.leftFrontMotorDirection = config.leftFrontDirection.toMotorDirection();
-        mConstants.rightFrontMotorDirection = config.rightFrontDirection.toMotorDirection();
-        mConstants.leftRearMotorDirection = config.leftRearDirection.toMotorDirection();
-        mConstants.rightRearMotorDirection = config.rightRearDirection.toMotorDirection();
-
-        //todo: add values for drivetrain hardware constants
-
-        drivetrain = new Mecanum(hardwareMap, mConstants);
-
-        PinpointConstants pinpointConstants = new PinpointConstants();
-        pinpointConstants.hardwareMapName = "pinpoint";
-        //todo: add values for pinpoint system hardware constants
+        PinpointConstants pinpointConstants = new PinpointConstants()
+                .hardwareMapName("pinpoint")
+                .strafePodX(-7.5)
+                .forwardPodY(4.5)
+                .distanceUnit(DistanceUnit.INCH)
+                .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+                .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+                .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
         localizer = new PinpointLocalizer(hardwareMap, pinpointConstants);
 
+        if(startFollower){
+            MecanumConstants mConstants = new MecanumConstants();
 
-        FollowerConstants followerConstants = new FollowerConstants();
+            mConstants.leftFrontMotorName = config.leftFrontName;
+            mConstants.rightFrontMotorName = config.rightFrontName;
+            mConstants.leftRearMotorName = config.leftRearName;
+            mConstants.rightRearMotorName = config.rightRearName;
 
-        //todo: tune follower constants
+            mConstants.leftFrontMotorDirection = config.leftFrontDirection.toMotorDirection();
+            mConstants.rightFrontMotorDirection(config.rightFrontDirection.toMotorDirection());
+            mConstants.leftRearMotorDirection = config.leftRearDirection.toMotorDirection();
+            mConstants.rightRearMotorDirection = config.rightRearDirection.toMotorDirection();
 
-        follower = new Follower(followerConstants, localizer, drivetrain);
+
+            mConstants.maxPower(1);
+
+            drivetrain = new Mecanum(hardwareMap, mConstants);
+
+            FollowerConstants followerConstants = new FollowerConstants()
+                    .mass(10.25);
+
+
+            //todo: tune follower constants
+
+            follower = new FollowerBuilder(followerConstants, hardwareMap)
+                    .pinpointLocalizer(pinpointConstants)
+                    .mecanumDrivetrain(mConstants)
+                    .build();
+        }
     }
 
     /**
@@ -61,14 +78,8 @@ public class DriveBase {
      * @param y the power to move forward/back with. Positive -> forward, Negative -> backward
      * @param turn the power to turn with. Positive -> turn right, Negative -> turn left
      */
-    public void moveUsingRR(double x, double y, double turn){
-//        setDriveSignal(new DriveSignal(
-//                new Pose2d(
-//                TRANSLATIONAL_VELOCITY_MULTIPLIER * y,
-//                TRANSLATIONAL_VELOCITY_MULTIPLIER * x,
-//                HEADING_VELOCITY_MULTIPLIER * -turn
-//                )
-//        ));
+    public void moveUsingPP(double x, double y, double turn){
+        //todo: IMPLEMENT
     }
 
     public void moveUsingPower(double x, double y, double turn){
@@ -81,20 +92,23 @@ public class DriveBase {
         double rightFront = ((y + x - turn) / denominator) * powerFactor;
         double rightRear = ((y - x - turn) / denominator) * powerFactor;
 
-        //setMotorPowers(leftFront, leftRear, rightRear, rightFront);
+        setMotorPowers(leftFront, leftRear, rightFront, rightRear);
     }
 
     public Pose getPoseSimple(){
-//        updatePoseEstimate();
-//        return Pose.from(super.getPoseEstimate());
-        return new Pose(0,0,0);
+        com.pedropathing.geometry.Pose pose = localizer.getPose();
+        return new Pose(pose.getX(), pose.getY(),pose.getHeading());
     }
 
     /**
      * Stops all motors. This is a shortcut method for <code>driveBase.setMotorPowers(0, 0, 0, 0)</code>`.
      */
     public void stop(){
-        //setMotorPowers(0,0,0,0);
+        setMotorPowers(0,0,0,0);
+    }
+
+    public void setMotorPowers(double lf, double rf, double lr, double rr){
+        drivetrain.runDrive(new double[]{rr, lf, -rf, lf});
     }
 
     public void setPowerFactor(double powerFactor){
