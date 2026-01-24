810ef5432a68fbe853ebe239db43aa79d4174e10
Callam Jomaa <apps@callamjomaa.com>
2026-01-23 20:11:15 -0500

    add localizer update call

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/DriveBase.java
@@ -94,6 +94,7 @@ public class DriveBase {
     }
 
     public Pose getPoseSimple(){
+        localizer.update();
         com.pedropathing.geometry.Pose pose = localizer.getPose();
         return new Pose(pose.getX(), pose.getY(),pose.getHeading());
     }
